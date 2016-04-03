acf11f50aa90dd59e6b49c9234a10fa5d98e1a8b
Dirk Hesse <herr.dirk.hesse@gmail.com>
2016-04-03 16:55:26 +0200

    Adding some more config storage/retrieving functionality.

--- tagalizer/daemon.py
+++ tagalizer/daemon.py
@@ -1,4 +1,5 @@
 """The daemon listening to Twitter."""
+import argparse
 
 class ConfigError(Exception):
     pass
@@ -7,3 +8,15 @@ def getConfig(mongo_client):
     result = mongo_client.tagalizer.config.find()
     if result == None:
         raise ConfigError
+    options = {}
+    for option in result:
+        if option['option_name'] in options:
+            raise ConfigError
+        options[option['option_name']] = option['value']
+    return options
+
+def storeConfig(mongo_client, config):
+    for option in config:
+        mongo_client.tagalizer.config.insert_one(
+            {'option_name': option,
+             'value': config[option]})
--- test_tagalyzer.py
+++ test_tagalyzer.py
@@ -3,25 +3,28 @@ import unittest
 from collections import defaultdict
 
 class MockDB(object):
-    """Mock object to simulate a MongoDB database."""
-    def find(*args, **kwargs):
-        return None
+    """Mongo database that can store stuff..."""
+    def __init__(self):
+        self.objects = []
+    def find(self):
+        if self.objects == []:
+            return None
+        return self.objects
+    def insert_one(self, what):
+        self.objects.append(what)
 
 class MongoMock(object):
     """Mock object to simulate a MonoClient instance."""
-    def __init__(self, level=0):
-        self.level = level
+    def __init__(self, db=None, level=0):
         self.children = defaultdict(
-            lambda: MongoMock(self.level + 1) if self.level < 1 else MockDB())
+            lambda: MongoMock(db, level + 1) if level < 1 else db or MockDB())
     def __getattr__(self, name):
         return self.children[name]
 
 class TestMongoMock(unittest.TestCase):
     """Tests the MongoDB mock class."""
-    def test_mock_class_has_level_zero(self):
-        self.assertEqual(MongoMock().level, 0)
-    def test_mock_collection_has_level_one(self):
-        self.assertEqual(MongoMock().foo.level, 1)
+    def test_mock_collection_is_mongo_mock(self):
+        self.assertEqual(type(MongoMock().foo), MongoMock)
     def test_mock_collection_produces_mock_dbs(self):
         self.assertEqual(type(MongoMock().foo.bar), MockDB)
     def test_returns_None(self):
@@ -32,6 +35,23 @@ class TestConfigManagement(unittest.TestCase):
     def test_get_config_throws_if_no_config_found(self):
         with self.assertRaises(td.ConfigError):
             td.getConfig(MongoMock())
+    def test_get_config_collects_options(self):
+        db = MockDB()
+        db.insert_one({'option_name': 'foo', 'value': 2})
+        db.insert_one({'option_name': 'bar', 'value': 3})
+        self.assertEqual(td.getConfig(MongoMock(db)),
+                         {'foo': 2, 'bar': 3})
+    def test_get_config_throws_if_options_exists_twice(self):
+        db = MockDB()
+        db.insert_one({'option_name': 'foo', 'value': 2})
+        db.insert_one({'option_name': 'foo', 'value': 3})
+        with self.assertRaises(td.ConfigError):
+            td.getConfig(MongoMock(db))
+    def test_store_config(self):
+        db = MockDB()
+        td.storeConfig(MongoMock(db), {'foo': 1, 'bar': 2})
+        self.assertEqual(db.objects, [{'option_name': 'foo', 'value': 1},
+                                      {'option_name': 'bar', 'value': 2}])
 
 if __name__ == '__main__':
     unittest.main()
